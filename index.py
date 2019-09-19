2407fd491c15ab4d76f32b1ccc1f1e9a659811b8
jdoe68877 <35254611+jdoe68877@users.noreply.github.com>
2019-09-19 10:20:08 +0400

    Revert "Update index.py"
    This reverts commit e14c7dd0a2314ec9c23b8e873fb5722c7be37193.

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ from io import StringIO
 import plotly
 import dash_auth
 
-import plotly.plotly as py
+import chart_studio.plotly as py
 import plotly.graph_objs as go
 
 global myauthenticateduser
